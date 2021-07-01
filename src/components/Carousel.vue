5cd66bca19b85d8afced747bf7ed9c79bc3b91a5
Ryan <mhogar98@gmail.com>
2021-07-01 14:44:59 -0400

    Add typescript plugin and implement carousel component

---
+++ src/components/Carousel.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button v-for="(slide, index) in slides" :key="slide.title" type="button" data-bs-target="#myCarousel" :data-bs-slide-to="index" :class="firstItemActive(index)" :aria-label="slide.title + ' Slide'"></button>
+    </div>
+    <div class="carousel-inner">
+      <div v-for="(slide, index) in slides" :key="slide.title" :class="'carousel-item ' + firstItemActive(index)">
+        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>{{ slide.title }}</h1>
+            <p>{{ slide.description }}</p>
+            <p>
+              <a class="btn btn-lg btn-primary" :href="slide.buttonLink.url" target="_blank">
+                {{ slide.buttonLink.text }}
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+/* Carousel base class */
+.carousel {
+  margin-bottom: 4rem;
+}
+/* Since positioning the image, we need to help out the caption */
+.carousel-caption {
+  bottom: 8rem;
+  z-index: 10;
+}
+
+/* Declare heights because of positioning of img element */
+.carousel-item {
+  height: 32rem;
+}
+.carousel-item > img {
+  /* position: absolute; */
+  /* background-position-x: center; */
+  /* background-position-y: top; */
+  min-width: 100%;
+  /*height: 32rem;*/
+}
+
+</style>
+
+<script lang="ts">
+import { PropType } from '@vue/runtime-core'
+import { Options, Vue } from 'vue-class-component'
+import Link from '../common/Link'
+
+export interface Slide {
+  title: string,
+  description: string,
+  image: string,
+  buttonLink: Link
+}
+
+@Options({
+  props: {
+    slides: Array as PropType<Slide[]>
+  }
+})
+export default class Carousel extends Vue {
+  slides!: Slide[]
+
+  firstItemActive (index: number): string {
+    return index === 0 ? 'active' : ''
+  }
+}
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Carousel :slides="carouselSlides" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Carousel, { Slide } from '../components/Carousel.vue'
 
-export default {
-  name: 'Home',
+@Options({
   components: {
-    HelloWorld
+    Carousel
   }
+})
+export default class Home extends Vue {
+  carouselSlides: Slide[] = [
+    {
+      title: 'Code Projects',
+      description: 'Interesting, nerdy, and always open-source.',
+      image: 'banner.png',
+      buttonLink: {
+        text: 'View them on GitHub',
+        url: 'https://github.com/mhogar'
+      }
+    },
+    {
+      title: 'Games',
+      description: 'Fun, creative, with little something for everyone.',
+      image: '',
+      buttonLink: {
+        text: 'Play them on Itch.io',
+        url: 'https://mhogar.itch.io/'
+      }
+    },
+    {
+      title: 'Animations',
+      description: 'Short, funny, and worth a watch.',
+      image: '',
+      buttonLink: {
+        text: 'Watch them on YouTube',
+        url: 'https://www.youtube.com/channel/UC4TeW-oe79vNo5ewMXJvr4w'
+      }
+    }
+  ]
 }
 </script>
